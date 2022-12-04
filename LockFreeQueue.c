9896850d8a626cb4a03216887d1b3f332d2f275a
pycai <545152168@qq.com>
2022-12-04 22:50:10 +0800

    using list and array to implement

--- LockFreeQueue.c
+++ LockFreeQueue.c
@@ -1,10 +1,20 @@
 #include "LockFreeQueue.h"
 
+#define NULL_ELEMENT 0xffffffffU
+
 typedef struct
 {
-	uint32_t size;
-	uint64_t startAndCount; // startIndex 4Byte, Count 4Byte
-	void* array[1];
+    uint32_t next;
+    uint32_t using;
+    uint64_t data;
+} LockFreeElement_t;
+
+typedef struct
+{
+    uint32_t head;
+    uint32_t tail;
+    uint32_t size;
+    LockFreeElement_t array[1];
 } LockFreeQueue_t;
 
 uint64_t lfqCreate(uint32_t size)
@@ -15,16 +25,23 @@ uint64_t lfqCreate(uint32_t size)
 		return 0;
 	}
 
-	uint32_t allSize = sizeof(LockFreeQueue_t) + (size - 1) * sizeof(void*);
-	LockFreeQueue_t* lfq = (LockFreeQueue_t*) malloc(sizeof(LockFreeQueue_t) + (size - 1) * sizeof(void*));
+	uint32_t allSize = sizeof(LockFreeQueue_t) + (size - 1) * sizeof(LockFreeElement_t);
+	LockFreeQueue_t* lfq = (LockFreeQueue_t*) malloc(allSize);
 	if (lfq == 0)
 	{
 		LOG_ERROR("malloc(%u) failed, errno[%d]", allSize, errno);
 		return 0;
 	}
 
-	lfq->size = size;
-	lfq->startAndCount = 0;
+    lfq->size = size;
+    for (uint32_t i = 0; i < size; i++)
+    {
+        lfq->array[i].next = NULL_ELEMENT;
+        lfq->array[i].using = 0;
+        lfq->array[i].data = 0;
+    }
+    lfq->head = lfq->tail = mallocElement(lfq);
+    
 	LOG_DEBUG("create LockFreeQueue with size[%u] success", size);
 	return (uint64_t)(long)lfq;
 }
@@ -44,26 +61,32 @@ int lfqPush(uint64_t handle, void* data)
 		return -1;
 	}
 
+    uint32_t element = mallocElement(lfq);
+    if (element == NULL_ELEMENT)
+    {
+        LOG_ERROR("queue full");
+        return -1;
+    }
+    lfq->array[element].data = (uint64_t)(long)data;
+    
+    uint32_t tail = lfq->tail;
+    LockFreeElement_t* array = lfq->array;
 	while (1)
 	{
-		uint64_t oldStartCount = lfq->startAndCount;
-		uint64_t start = oldStartCount >> 32;
-		uint64_t count = oldStartCount & 0xffffffffULL;
-		if (count == lfq->size)
-		{
-			LOG_ERROR("queue full");
-			return -1;
-		}
-
-		count++;
-		uint64_t index = (start + count - 1) % lfq->size;
-		uint64_t newStartCount = (start << 32 | count);
-		if (__sync_bool_compare_and_swap(&lfq->startAndCount, oldStartCount, newStartCount))
-		{
-			lfq->array[index] = data;
-			LOG_DEBUG("lfqPush success");
-			return 0;
-		}
+        uint64_t tmp = __sync_val_compare_and_swap(&array[tail].data, 0, (uint64_t)(long)data);
+        if (tmp == 0)
+        {
+            LOG_DEBUG("lfqPush success");
+            freeElement(element);
+            return 0;
+        }
+        
+        tail = __sync_val_compare_and_swap(&array[tail].next, NULL_ELEMENT, element);
+        if (tail == NULL_ELEMENT)
+        {
+            LOG_DEBUG("lfqPush success");
+            return 0;
+        }
 	}
 
 	LOG_ERROR("this is impossible");
@@ -86,28 +109,27 @@ int lfqPop(uint64_t handle, void** data)
 	}
 	*data = 0;
 
+    LockFreeElement_t* array = lfq->array;
 	while (1)
 	{
-		uint64_t oldStartCount = lfq->startAndCount;
-		uint64_t start = oldStartCount >> 32;
-		void* tmpData = lfq->array[start];
-		uint64_t count = oldStartCount & 0xffffffffULL;
-		if (count == 0)
-		{
-			LOG_ERROR("queue empty");
-			return -1;
-		}
-
-		uint64_t index = start;
-		count--;
-		start = (start + 1) % lfq->size;
-		uint64_t newStartCount = (start << 32 | count);
-		if (__sync_bool_compare_and_swap(&lfq->startAndCount, oldStartCount, newStartCount))
-		{
-			*data = tmpData;
-			LOG_DEBUG("lfqPop success");
-			return 0;
-		}
+        uint32_t head = lfq->head;
+        void* ret = array[head].data;
+        uint32_t next = array[head].next;
+        uint32_t tail = lfq->tail;
+    
+        if (head == tail)
+        {
+            LOG_ERROR("queue empty");
+            return -1;
+        }
+        
+        uint32_t tmp = __sync_val_compare_and_swap(&lfq->head, head, next);
+        if (tmp == head)
+        {
+            freeElement(head);
+            *data = ret;
+            return 0;
+        }
 	}
 
 	LOG_ERROR("this is impossible");
